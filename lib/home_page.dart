6e427ae21b9f28a38d0e7a477a52e622e481a846
Maruf Robin <maruf.robin@northsouth.edu>
2023-01-06 22:37:10 +0600

    homepage added

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fucking Music App"),
+        leading:
+            IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow_rounded)),
+      ),
+    );
+  }
+}
