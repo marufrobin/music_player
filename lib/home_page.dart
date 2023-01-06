531cdb9aaec8a2175e4dffe7faf08d8c848a37d0
Maruf Robin <maruf.robin@northsouth.edu>
2023-01-07 01:09:43 +0600

    song are playing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +9,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  Duration _duration = Duration();
+  Duration _postion = Duration();
+  String url =
+      "https://github.com/truecoder-001/audio-file-play-stop-pause-resume/raw/main/theme_01.mp3";
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    audioPlayer.onDurationChanged.listen((event) {
+      setState(() {
+        _duration = event;
+      });
+    });
+    audioPlayer.onPositionChanged.listen((event) {
+      setState(() {
+        _postion = event;
+      });
+    });
+    audioPlayer.setSourceUrl(url);
   }
 
   @override
@@ -19,8 +37,11 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Fucking Music App"),
-        leading:
-            IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow_rounded)),
+        leading: IconButton(
+            onPressed: () async {
+              await audioPlayer.play(UrlSource(url));
+            },
+            icon: Icon(Icons.play_arrow_rounded)),
       ),
     );
   }
